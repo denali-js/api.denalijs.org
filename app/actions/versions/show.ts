b9a62476ba83d5caa4686c67f4ad0e0e32415e19
Dave Wasmer <davewasmer@gmail.com>
2018-02-28 15:59:02 -0700

    add debug output to version endpoint

--- app/actions/versions/show.ts
+++ app/actions/versions/show.ts
@@ -1,3 +1,4 @@
+import { inspect } from 'util';
 import assert from 'assert';
 import { Errors } from '@denali-js/core';
 import ApplicationAction from '../application';
@@ -6,7 +7,8 @@ import Version from '../../models/version';
 export default class ShowVersion extends ApplicationAction {
 
   async respond({ query }: { query: { addon: string, version: string} }) {
-    assert(query.version, 'You must include which version to fetch, i.e. denali:0.1.0');
+    assert(query.addon, `You must include which addon to fetch: ${ inspect(query) }`);
+    assert(query.version, `You must include which version to fetch: ${ inspect(query) }`);
     let version = await Version.queryOne({ addon_id: query.addon, version: query.version });
     if (!version) {
       throw new Errors.NotFound(`Version not found (addon: "${ query.addon }", version: "${ query.version }")`);
